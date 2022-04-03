b25430b834a1d4dcc88dab67835d30f27c2ce7cc
Matheus Aguilar <matheus.rod.aguilar@gmail.com>
2022-04-03 15:17:59 -0300

    Adicionado integracao com Swagger para ajudar no desenvolvimento back

--- Startup.cs
+++ Startup.cs
@@ -6,7 +6,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using PharmaSystem.DataModel.DataContext;
+using System;
 
 namespace PharmaSystem
 {
@@ -21,29 +23,38 @@ namespace PharmaSystem
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
             services.AddDbContext<PharmacyContext>(options => options.UseSqlServer(ConnectionString));
-            // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "An ASP.NET Core Web API for managing ToDo items"
+                });
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -63,10 +74,14 @@ namespace PharmaSystem
                     pattern: "{controller}/{action=Index}/{id?}");
             });
 
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("./swagger", "v1");
+                options.RoutePrefix = string.Empty;
+            });
+
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
 
                 spa.Options.SourcePath = "ClientApp";
 
